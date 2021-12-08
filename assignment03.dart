2eafa12bc4395d0a256daec399f1aebcd16c54b0
Vijay Kumar <95156170+vijaykumar361@users.noreply.github.com>
2021-12-08 10:04:18 +0500

    Add files via upload

---
+++ assignment03.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+void main() {
+  // print("");
+  // print("Task_01");
+  // print("Marksheet");
+  // print("Enter The First Subject:");
+  // var FS = stdin.readLineSync();
+  // print("Enter The Second Subject:");
+  // var SS = stdin.readLineSync();
+  // print("Enter The Third Subject:");
+  // var TS = stdin.readLineSync();
+  // print("Your Total Subject: $FS " " $SS " " $TS");
+  // int TSM = 100;
+  // print("Each Subject Take: $TSM");
+  // print("");
+  // print("Enter The First Subject Marks:");
+  // var FM = int.parse(stdin.readLineSync()!);
+  // print("Enter The Second Subject Marks:");
+  // var SM = int.parse(stdin.readLineSync()!);
+  // print("Enter The Third Subject Marks:");
+  // var TM = int.parse(stdin.readLineSync()!);
+  // int OM = FM + SM + TM;
+  // print("Your Obtained Marks Are:: $OM");
+  // double Percentage = OM / 300 * 100;
+  // print("Your Percentage Is: $Percentage");
+
+  // print("");
+  // print("Task_02");
+  // print("Please Enter The City Name:");
+  // var city = stdin.readLineSync();
+  // if (city == "Karachi") {
+  //   print("$city => The City Of Light:");
+  // } else {
+  //   print("$city => Is Not City Of Light");
+  // }
+
+  // print("");
+  // print("Task_03");
+  // print("Please Enter The Gender:");
+  // var gender = stdin.readLineSync();
+  // if (gender == "Male") {
+  //   print("Good Morning Sir:");
+  // } else if (gender == "Female") {
+  //   print("Good Morning Ma'am:");
+  // }
+
+  // print("");
+  // print("Task_04");
+  // print("Enter remaining Fuel In Your Car:(In Liters)");
+  // double fuel = double.parse(stdin.readLineSync()!);
+  // if (fuel < 0.25) {
+  //   print("Please Refull The Fuel:");
+  // } else {
+  //   print("No Need To Refull The Fuel");
+  // }
+
+  // print("");
+  // print("Task_05");
+  // print("Enter The Temperature:");
+  // int Tem = int.parse(stdin.readLineSync()!);
+  // if (Tem > 40) {
+  //   print("$Tem => It Is Too Hot Outside:");
+  // } else if (Tem > 30) {
+  //   print("$Tem => The Weather Is Normal Today:");
+  // } else if (Tem > 20) {
+  //   print("$Tem => Today's Weather Is Cool:");
+  // } else if (Tem > 10) {
+  //   print("$Tem => OMG! Today's weather Is So Cool:");
+  // }
+
+  // print("");
+  // print("Task_06");
+  // print("Enter the FIrst Number:");
+  // var f = int.parse(stdin.readLineSync()!);
+  // print("Enter The Second Number:");
+  // var s = int.parse(stdin.readLineSync()!);
+  // if (f > s) {
+  //   print("First Number Is Greater Than Second Number:");
+  // } else if (f == s) {
+  //   print("Both Numbers Are Equal:");
+  // } else if (f < s) {
+  //   print("Second Number Is Greater Than First Number:");
+  // }
+
+  // print("");
+  // print("Task_07");
+  // print("Enter The Any Character");
+  // var ch = stdin.readLineSync();
+}
